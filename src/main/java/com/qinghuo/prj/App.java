89f28ae59a6b85abeb685594a0af173b774f5b02
hehuan <hehuan_work@126.com>
2019-05-11 21:00:50 +0800

    定时任务提交

--- src/main/java/com/qinghuo/prj/App.java
+++ src/main/java/com/qinghuo/prj/App.java
@@ -1,13 +1,27 @@
 package com.qinghuo.prj;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(10);
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        executor.scheduleWithFixedDelay(new MyTask(),5,5, TimeUnit.SECONDS);
+    }
+
+    static class MyTask implements Runnable {
+        private int i = 10;
+        @Override
+        public void run() {
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + (i++));
+        }
     }
 }
